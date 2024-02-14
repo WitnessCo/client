3c134b33c43c6651ae2129449d70dc1af7902950
sinasab <sinasabet81@gmail.com>
2024-02-13 22:01:36 -0800

    bump

--- src/example.ts
+++ src/example.ts
@@ -6,7 +6,7 @@ const witness = new WitnessClient();
 // Helper method for getting the hash of a string.
 const sampleString = `Check the chain! @ ${Date.now()}`;
 const leafHash = witness.hash(sampleString);
-console.log(`Timestamping leaf ${leafHash}`);
+console.log(`Timestamping leaf hahs ${leafHash}`);
 
 // Post the leafHash to the server.
 await witness.postLeaf(leafHash);
