29e65fd556377cae3df89e890c6558cd8f96c869
sinasab <sinasabet81@gmail.com>
2024-04-08 14:12:18 -0700

    format

--- src/contracts/utils.ts
+++ src/contracts/utils.ts
@@ -1,22 +1,22 @@
 import {
-  arbitrum,
-  arbitrumSepolia,
-  base,
-  baseSepolia,
-  mainnet,
-  optimism,
-  optimismSepolia,
-  sepolia,
+	arbitrum,
+	arbitrumSepolia,
+	base,
+	baseSepolia,
+	mainnet,
+	optimism,
+	optimismSepolia,
+	sepolia,
 } from "viem/chains";
 
 export const supportedChains = [
-  base,
-  mainnet,
-  optimism,
-  baseSepolia,
-  sepolia,
-  optimismSepolia,
-  arbitrumSepolia,
+	base,
+	mainnet,
+	optimism,
+	baseSepolia,
+	sepolia,
+	optimismSepolia,
+	arbitrumSepolia,
 ] as const;
 
 export type SupportedChainType = (typeof supportedChains)[number];
@@ -25,7 +25,7 @@ export const supportedChainIds = supportedChains.map((c) => c.id);
 export type SupportedChainIdType = (typeof supportedChainIds)[number];
 
 export const getSupportedChainFromChainId = (chainId: number) => {
-  const res = supportedChains.find((c) => c.id === chainId);
-  if (!res) throw new Error(`Unsupported chain: ${chainId}`);
-  return res satisfies SupportedChainType;
+	const res = supportedChains.find((c) => c.id === chainId);
+	if (!res) throw new Error(`Unsupported chain: ${chainId}`);
+	return res satisfies SupportedChainType;
 };
